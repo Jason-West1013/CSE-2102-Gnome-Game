b1e34f276a0a1beccfed638a2ae0651c20c85c49
Mitchell Lombardi <Lombardi.Mitchell@gmail.com>
2017-01-23 14:22:26 -0500

    Test

--- Codename-Rip/src/Game/Game.java
+++ Codename-Rip/src/Game/Game.java
@@ -13,11 +13,11 @@ public class Game extends Canvas implements Runnable {
 	
 	public synchronized void start() {}
 	
-	@Override
 	public void run() {}
 
 	public static void main(String[] args) {
 		new Game();
+		
 	}
 
 }
