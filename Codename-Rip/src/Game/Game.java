5ee84298cb721584e2b3d4f18a64fd37939c5c80
Mitch <Mitch@67.221.91.141>
2017-01-24 14:35:13 -0500

    Added Game Loop

--- Codename-Rip/src/Game/Game.java
+++ Codename-Rip/src/Game/Game.java
@@ -4,15 +4,67 @@ import java.awt.Canvas;
 
 public class Game extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
+	private boolean running = false;
+
+	private Thread thread;
+
 	public static final int WIDTH = 640, HEIGHT = 480;
-	
+
 	public Game() {
 		new Window(WIDTH, HEIGHT, "Swordman", this);
+		this.requestFocus();
 	}
-	
-	public synchronized void start() {}
-	
-	public void run() {}
+
+	public synchronized void start() {
+		running = true;
+
+		thread = new Thread(this);
+		thread.start();
+	}
+
+	public void run() {
+		long lastTime = System.nanoTime();
+		long timer = System.currentTimeMillis();
+
+		double amountOfTicks = 60.0;
+		double nsPerTick = 1000000000 / amountOfTicks;
+		double delta = 0.0;
+
+		int updates = 0;
+		int frames = 0;
+
+		while (running) {
+			long now = System.nanoTime();
+
+			delta += (now - lastTime) / nsPerTick;
+			lastTime = now;
+
+			while (delta >= 1) {
+				tick();
+				updates++;
+
+				delta--;
+			}
+
+			render();
+			frames++;
+
+			if ((System.currentTimeMillis() - timer) >= 1000) {
+				System.out.println("UPS: " + updates + ", FPS: " + frames);
+
+				updates = 0;
+				frames = 0;
+
+				timer += 1000;
+			}
+		}
+	}
+
+	public void tick() {
+	}
+
+	public void render() {
+	}
 
 	public static void main(String[] args) {
 		new Game();
--- Codename-Rip/src/Game/Window.java
+++ Codename-Rip/src/Game/Window.java
@@ -1,26 +1,26 @@
 package Game;
+
 import java.awt.Canvas;
 import java.awt.Dimension;
 import javax.swing.JFrame;
 
 public class Window extends Canvas {
-
 	private static final long serialVersionUID = 1L;
-	
+
 	public Window(int width, int height, String title, Game game) {
 		JFrame frame = new JFrame(title);
-		
+
 		frame.setMaximumSize(new Dimension(width, height));
-		frame.setMinimumSize(new Dimension(width,height));
-		frame.setPreferredSize(new Dimension(width,height));
-		
-		frame.add(game);
-		frame.requestFocus();
+		frame.setMinimumSize(new Dimension(width, height));
+		frame.setPreferredSize(new Dimension(width, height));
+
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLocationRelativeTo(null); 
+		frame.setLocationRelativeTo(null);
 		frame.setResizable(false);
+
+		frame.add(game);
 		frame.setVisible(true);
-		
+
 		game.start();
 	}
 }
